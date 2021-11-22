c11bb4bd84b61de7914d094c809ff4342410ebe2
Рамиль Ракаев <CHICO5Listo4@gmail.com>
2021-11-22 16:48:22 +0300

    .

---
+++ WebApplicationTest/wwwroot/js/app.jsx
@@ -0,0 +1,9 @@
+﻿class Hello extends React.Component {
+    render() {
+        return <h1>Привет, React</h1>;
+    }
+}
+ReactDom.render(
+    <Hello />,
+    document.getElementById("content")
+);
