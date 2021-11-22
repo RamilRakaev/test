6f481d2a848586581cfe3aeb2ded2ae57b002603
Рамиль Ракаев <CHICO5Listo4@gmail.com>
2021-11-22 14:15:29 +0300

    .

---
+++ LinkLibrary/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace LinkLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            LinksManager manager = new (path);
+            manager.OutputLinks = (SourceReferencing[] reference) =>
+            {
+                foreach (var source in reference)
+                {
+                    Console.WriteLine($"{source.Titles}\nСсылки:");
+                    foreach(var link in source.Links)
+                    {
+                        Console.WriteLine($"{link}");
+                    }
+                }
+            };
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class SourceReferencing
+    {
+        public string Titles { get; set; }
+        public string[] Links { get; set; }
+    }
+
+    public class LinksManager
+    {
+        public Action<SourceReferencing[]> OutputLinks;
+        private readonly string _path;
+
+        public LinksManager(string path)
+        {
+            _path = path;
+        }
+
+        public ReadResult ReadPageLinks()
+        {
+            if (File.Exists(_path))
+            {
+                using (StreamReader reader = new (_path))
+                {
+                    var references = LinksParsing.Parse(reader.ReadToEnd());
+                    OutputLinks(references);
+                    return ReadResult.Success;
+                }
+            }
+            return ReadResult.FileNotFound;
+        }
+
+        public enum ReadResult { FileNotFound, Success }
+    }
+
+    internal class LinksParsing
+    {
+        const string separator = "###";
+        const string and = "&&&";
+
+        public static SourceReferencing[] Parse(string page)
+        {
+            var lines = page.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+            var references = new List<SourceReferencing>();
+            foreach(var line in lines)
+            {
+                if (line.Contains("separator"))
+                {
+                    var sourceReferencing = line.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                    var titles = sourceReferencing[0];
+                    var links = sourceReferencing[1];
+                    foreach (var title in links.Split(and, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        var reference = new SourceReferencing()
+                        {
+                            Titles = title,
+                            Links = titles.Split(and, StringSplitOptions.RemoveEmptyEntries),
+                        };
+                        references.Add(reference);
+                    }
+                }
+            }
+            return references.ToArray();
+        }
+    }
+
+}
--- test/Program.cs
+++ test/Program.cs
@@ -11,12 +11,127 @@ using System.Threading.Tasks;
 
 namespace test
 {
+    public class Supchik
+    {
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+
+        }
+        public string FullName { get; set; }
+
+        public static int Add(params int[] terms)
+        {
+            int result = 0;
+            foreach(var term in terms)
+            {
+                result += term;
+            }
+            return result;
+        }
+    }
+    public class Node
+    {
+        public Node Parent { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Node> Children { get; set; }
+
+        public void GetChildrenNames(in Action<string> action)
+        {
+            GetChildrenNames(action, this);
+        }
+
+        private void GetChildrenNames(in Action<string> action, Node node)
+        {
+            action($"Наименование {node.Name}. Уровень {node.GetLevel()}");
+            if (node.Children == null)
+            {
+                return;
+            }
+            foreach(var child in node.Children)
+            {
+                GetChildrenNames(action, child);
+            }
+        }
+        
+    }
+
     public class Program
     {
         static async Task Main(string[] args)
         {
+            #region
+            Node parent = new ();
+            parent.Name = "Fucking parent";
 
+            Node child1 = new()
+            {
+                Name = "child1",
+                Parent = parent
+            };
+
+            Node child2 = new()
+            {
+                Name = "child2",
+                Parent = child1
+            };
+
+            Node child3 = new()
+            {
+                Name = "child3",
+                Parent = child2
+            };
+
+            Node child4 = new()
+            {
+                Name = "child4",
+                Parent = child3
+            };
+            #endregion
+
+            //Console.WriteLine(child4.GetLevel());
+
+            #region
+            parent.Children = new Node[]
+            {
+                child1
+            };
+
+            child1.Children = new Node[]
+            {
+                child2, child3
+            };
+
+            child2.Children = new Node[]
+            {
+                child3
+            };
+
+            child3.Children = new Node[]
+            {
+                child4
+            };
+            #endregion
+
+            parent.GetChildrenNames((string name) => Console.WriteLine(name));
             Console.ReadLine();
+        }
+    }
+
+    public static class NodeExtensions
+    {
+        public static int GetLevel(this Node node)
+        {
+            return GetLevel(node, 1);
+        }
+
+        private static int GetLevel(Node node, int level)
+        {
+            if (node.Parent == null)
+            {
+                return level;
+            }
+            return GetLevel(node.Parent, ++level);
         }
     }
 
