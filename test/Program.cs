081b4b17ab7341eb6ed1411e9ac7b3c2f8e7fd4e
Рамиль Ракаев <ramrg546@gmail.com>
2022-05-11 15:58:47 +0300

    .

--- test/Program.cs
+++ test/Program.cs
@@ -1,39 +1,20 @@
-﻿using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
-using System;
-using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace test
+namespace ConsoleApplication1
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("bla bla 2aa23r".GetTextAfterSubstring("2aa"));
-            Console.ReadKey();
-        }
-    }
-
-    public static class Text
-    {
-        public static string GetTextAfterSubstring(this string text, string character, int number = 1)
-        {
-            int index = 0;
-            index = text.IndexOf(character) + character.Length;
-            if(text.Length == index)
+            Dictionary<int, string> dict = new()
             {
-                return string.Empty;
-            }
-            return text[index..].Trim(' ');
+                { 1, "str1" },
+                { 2, "str2" },
+            };
+
+            dict[10] = "key";
         }
+
     }
 }
-
